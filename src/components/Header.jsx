c67f575d9fd8b3215ea8cf9f08a75a883ec1f659
badisbms <79091956+badisbms@users.noreply.github.com>
2021-11-09 10:50:37 +0100

    ajout component header + css

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <p>tutu</p>
+        </div>
+    );
+};
+
+export default Header;
