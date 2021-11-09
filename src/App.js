553f114061556be8c2cf237c255616b5f513a35b
badisbms <79091956+badisbms@users.noreply.github.com>
2021-11-09 12:06:03 +0100

    ajout card bootstrap + requete axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
+import './styles/App.css';
 import Header from './components/Header';
+import MeteoCard from './components/MeteoCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MeteoCard />
+   
     </div>
   );
 }
